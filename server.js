107855aded7e56d7a7b7b73a0d7ac0cdf2a1ccbb
Anubhav Srivastava <srivastavaanubhav764@gmail.com>
2022-01-27 15:21:43 +0530

    Version -v1

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./moels/shortUrl')
+const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortner',{
+	useNewUrlParser:true
+})
+
+
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:false}));
+
+app.get('/', async function(req,res){
+const urls = await ShortUrl.find()
+	res.render('index',{urls:urls});
+})
+
+
+
+app.post('/shortUrl' , async function(req,res){
+	   await ShortUrl.create({full:req.body.fullUrl})
+	   res.redirect('/');
+})
+
+app.get('/:shortUrl', async function(req,res){
+	const foundUrl = await ShortUrl.findOne({short : req.params.shortUrl});
+	if(!foundUrl){
+		return res.sendStatus(404);
+	}
+
+	foundUrl.clicks++;
+	foundUrl.save();
+	res.redirect(foundUrl.full);
+})
+
+app.listen(5000,()=>console.log("Server has started"));
